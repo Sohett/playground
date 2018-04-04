dc3bf17cb473a1f2f12d74365dcd92f5e94f4f87
Thomas Sohet <sohet.Thomas@gmail.com>
2018-04-04 15:03:37 +0200

    rbenv in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,17 @@ lock "~> 3.10.1"
 set :application, "playground"
 set :repo_url, "git@github.com:Sohett/playground.git"
 
+# config/deploy.rb
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.3.0'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails puma}
+set :rbenv_roles, :all # default value
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
